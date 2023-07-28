077666bb1480402d78bb54243eb8c0cc4fdb0ee3
karthaaz <karthickyash@gmail.com>
2023-07-28 23:35:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const apiKey="386e41d8b0ebc0267c0bb919844d2044"
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl+city+`&appid=${apiKey}`);
+if(response.status==404){
+    document.querySelector(".error").style.display="block"
+    document.querySelector(".weather").style.display="none"
+}
+else{
+    var data = await response.json();
+    
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML=data.wind.speed +" km/h";
+    document.querySelector(".weather").style.display="block"
+    document.querySelector(".error").style.display="none"
+}
+
+
+}
+
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value);
+    
+    })
